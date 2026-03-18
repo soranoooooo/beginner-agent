76515075ac2379b498d5110436030684baa1a131
psy <sorano7094@gmail.com>
2026-03-18 15:00:09 +0800

    add calculator tool and test

---
+++ app/test_tool.py
@@ -0,0 +1,4 @@
+from tools.calculator import add_numbers
+
+result = add_numbers(3, 5)
+print("计算结果：", result)
---
+++ tools/calculator.py
@@ -0,0 +1,5 @@
+def add_numbers(a, b):
+    """
+    返回两个数字的和
+    """
+    return a + b
